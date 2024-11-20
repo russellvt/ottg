e8aa99c8c3e2be9f8461e67646dbc1342b9fd313
Russell VT <russellvt@gmail.com>
2024-11-20 01:10:32 -0800

    Model for list Items and associated migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+'''
+Models
+
+Ref:
+ - https://docs.djangoproject.com/en/5.1/ref/models/fields/
+'''
+
 # Create your models here.
+class Item(models.Model):
+    '''Item Table'''
+    text = models.TextField(default="")
