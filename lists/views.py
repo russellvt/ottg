e4858c62dc27dd74b418431c39bdf46ffba0b372
Russell VT <russellvt@gmail.com>
2024-11-20 03:39:25 -0800

    New Lists Tests

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,13 @@
+'''
+List Views
+
+TODO:
+* Adjust model so that items are associated with different lists
+* Add unique URLs for each list
+* Add a URL for creating a new list via POST
+* Add URLs for adding a new item to an existing list via POST
+'''
+
 from django.shortcuts import redirect, render
 from lists.models import Item
 
