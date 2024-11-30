ec1365ef00e846c5e21988798e392ab83f9cb06c
Russell VT <russellvt@gmail.com>
2024-11-30 04:16:31 -0800

    Move Lists URLs in to App

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+from django.urls import include, path
 
-from lists import views
+from lists import views as list_views
 
 urlpatterns = [
-    path("", views.home_page, name="home"),
-    path("lists/new", views.new_list, name="new_list"),
-    path("lists/<int:list_id>/", views.view_list, name="view_list"),
-    path("lists/<int:list_id>/add_item", views.add_item, name="add_item"),
+    path("", list_views.home_page, name="home"),
+    path("lists/", include("lists.urls"))
     # path('admin/', admin.site.urls),
 ]
